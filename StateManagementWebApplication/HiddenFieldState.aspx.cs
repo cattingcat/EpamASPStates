0b74f566240d65e0b2ec62b7a14d9a259942df4a
zxc123zxc <mark.martynov@yahoo.com>
2014-06-02 00:12:30 +0400

    hidden field state added

---
+++ StateManagementWebApplication/HiddenFieldState.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StateManagementWebApplication
+{
+    public partial class HiddenFieldState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Hello world!
+            hiddenField.Value = "SGVsbG8gd29ybGQh";
+        }
+    }
+}
